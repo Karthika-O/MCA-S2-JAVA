c44b2373914b38fd2faa0938cc6eb3cee26dd064
Karthika-O <147071387+Karthika-O@users.noreply.github.com>
2024-04-18 12:17:11 +0530

    Add files via upload
    OOPS Lab prgm 7

---
+++ String_m.java
@@ -0,0 +1,27 @@
+public class String_m 
+{
+    String str;
+    String_m(String s)
+    {
+        this.str=s;
+    }
+    void manipulations()
+    {
+        String value=this.str;
+        System.out.println("String Length is : "+value.length());
+        System.out.println("String concatenation : "+value.concat(" concatenated"));
+        System.out.println("Index of character 's' : "+value.indexOf('s'));
+        System.out.println("Character at position 5 : "+value.charAt(5));
+        System.out.println("Compare to 'home next' : "+value.compareTo("home next"));
+        System.out.println("Contains Sequence 'ome' : "+value.contains("ome"));
+        System.out.println("Ends with character 't' : "+value.endsWith("t"));
+        System.out.println("Replace 'text' with 'words' : "+value.replace("text", "words"));
+        System.out.println("Convert to Lowercase : "+value.toLowerCase());
+        System.out.println("Convert to Uppercase : "+value.toUpperCase());
+    }
+    public static void main(String arg[])
+    {
+        String_m s=new String_m("some text");
+        s.manipulations();
+    }
+}
