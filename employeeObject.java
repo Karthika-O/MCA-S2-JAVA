f0b6b2a3f8f5d1b55aa3de3303d6decb704b8301
Karthika-O <147071387+Karthika-O@users.noreply.github.com>
2024-04-18 13:17:42 +0530

    Add files via upload

---
+++ employeeObject.java
@@ -0,0 +1,52 @@
+import java.util.*;  
+class employeeObject
+{
+    int eNo;
+    String eName;
+    int eSalary;
+
+    public void read()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter ID : "); 
+        eNo = sc.nextInt();
+        System.out.print("Enter Name : "); 
+        eName = sc.next();
+        System.out.print("Enter monthly salary : "); 
+        eSalary = sc.nextInt();
+    }
+
+    public void display(){
+        System.out.println("Name : "+ eName );
+    }
+    public static void main(String []args)
+    { 
+        Scanner sc= new Scanner(System.in);
+        int i,n;
+        int No;
+        System.out.println("Enter the number of objects to be created");
+        n=sc.nextInt();
+        employeeObject emp[] = new employeeObject[n];
+        for(i=0;i<n;i++)
+        {
+            emp[i] = new employeeObject();
+            emp[i].read();
+        }
+        System.out.println("Search"); 
+        System.out.print("Enter ID : "); 
+        No =sc.nextInt();
+            for(i=0;i<n;i++)
+            {
+                if(emp[i].eNo == No)
+                {
+                    emp[i].display();
+                    break;
+                }
+                else
+                {
+                    System.out.println("Employee ID mismatch");
+                    break;
+                }
+            }
+    }
+}
