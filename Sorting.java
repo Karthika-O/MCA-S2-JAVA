f91611ebdb5a97d63d09ed5efc31af1c19263c1d
Karthika-O <147071387+Karthika-O@users.noreply.github.com>
2024-04-18 11:50:29 +0530

    Create Sorting.java
    OOPS Lab prgm 6

---
+++ Sorting.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Sorting 
+{
+    String str;
+    public void get_string() 
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a string value: ");
+        this.str = s.nextLine();
+    }
+    public void sort_print() 
+    {
+        char charArray[] = str.toCharArray();
+        Arrays.sort(charArray);
+        String sortedString = "";
+        for (int i = 0; i < charArray.length; i++) 
+        {
+            sortedString += charArray[i];
+            if (i != charArray.length - 1) 
+            {
+                sortedString += " ";
+            }
+        }
+        System.out.println("Sorted String is "+sortedString);
+    }
+    public static void main(String args[]) 
+    {
+        Sorting s = new Sorting();
+        s.get_string();
+        s.sort_print();
+    }
+}
